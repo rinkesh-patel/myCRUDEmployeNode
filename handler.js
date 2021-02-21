3f01a097375c1568e9b89f7210d8a644ee296036
patel <patelrinkeshm@gmailcom>
2021-02-20 21:49:33 -0600

    change local var

--- handler.js
+++ handler.js
@@ -159,12 +159,12 @@ module.exports.getWeather = (event, context, callback) => {
   let apiKey = '28686699b8b52a829bfc61fc5621a0c4';
   let city = '75071';
   let url = `https://api.openweathermap.org/data/2.5/weather?zip=75071&appid=28686699b8b52a829bfc61fc5621a0c4&units=Imperial`
-  
+  let weather;
   request(url, function (err, response, body) {
     if(err){
-      console.log('error:', error);
+      console.log('error:', JSON.parse(err));
     } else {
-      let weather = JSON.parse(body)
+      weather = JSON.parse(body)
       let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
       console.log(message);
     }
