a8905e31528e2a2ab971a04cbd36b2ad43a40f60
patel <patelrinkeshm@gmailcom>
2021-02-20 20:55:10 -0600

    all templates

--- handler.js
+++ handler.js
@@ -52,3 +52,98 @@ module.exports.createPost = (event, context, callback) => {
     })
     .catch((err) => response(null, response(err.statusCode, err)));
 };
+
+// Get all posts
+module.exports.getAllPosts = (event, context, callback) => {
+  return db
+    .scan({
+      TableName: employeesTable
+    })
+    .promise()
+    .then((res) => {
+      callback(null, response(200, res.Items.sort(sortByDate)));
+    })
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
+// Get number of posts
+module.exports.getPosts = (event, context, callback) => {
+  const numberOfPosts = event.pathParameters.number;
+  const params = {
+    TableName: employeesTable,
+    Limit: numberOfPosts
+  };
+  return db
+    .scan(params)
+    .promise()
+    .then((res) => {
+      callback(null, response(200, res.Items.sort(sortByDate)));
+    })
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
+// Get a single post
+module.exports.getPost = (event, context, callback) => {
+  const id = event.pathParameters.id;
+
+  const params = {
+    Key: {
+      id: id
+    },
+    TableName: employeesTable
+  };
+
+  return db
+    .get(params)
+    .promise()
+    .then((res) => {
+      if (res.Item) callback(null, response(200, res.Item));
+      else callback(null, response(404, { error: 'Post not found' }));
+    })
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
+// Update a post
+module.exports.updatePost = (event, context, callback) => {
+  const id = event.pathParameters.id;
+  const reqBody = JSON.parse(event.body);
+  const { body, title } = reqBody;
+
+  const params = {
+    Key: {
+      id: id
+    },
+    TableName: employeesTable,
+    ConditionExpression: 'attribute_exists(id)',
+    UpdateExpression: 'SET title = :title, body = :body',
+    ExpressionAttributeValues: {
+      ':firstName': firstName,
+      ':lastName': lastName
+    },
+    ReturnValues: 'ALL_NEW'
+  };
+  console.log('Updating');
+
+  return db
+    .update(params)
+    .promise()
+    .then((res) => {
+      console.log(res);
+      callback(null, response(200, res.Attributes));
+    })
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
+// Delete a post
+module.exports.deletePost = (event, context, callback) => {
+  const id = event.pathParameters.id;
+  const params = {
+    Key: {
+      id: id
+    },
+    TableName: employeesTable
+  };
+  return db
+    .delete(params)
+    .promise()
+    .then(() =>
+      callback(null, response(200, { message: 'Post deleted successfully' }))
+    )
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
